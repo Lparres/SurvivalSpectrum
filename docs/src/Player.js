2fed0b2fd2dd9a483ac4d2a91cd2e56c73e9c8e8
VerXxuxZ <juserr04@ucm.es>
2023-10-30 02:40:38 +0100

    Me faltaba el player
    La clase Player y arreglos a Enemy

--- docs/src/Player.js
+++ docs/src/Player.js
@@ -1,27 +1,36 @@
-// clase de la que heredan los demás scripts de los enemigos
-export default class Enemy extends Phaser.GameObjects.Sprite
+export default class Player extends Phaser.GameObjects.Sprite
 {
     /*
     *Las variable con barra baja son las de la clase, si barra baja son las variables de la sobrecarga
     *Scene es la escena a la que pertenecen los enemigos
     *x es la coordenada x del sprite
     *y es la coordenada y del sprite
-    *vida es la vida inicial del Enemigo
+    *life es la vida inicial del jugador
     *AtkCD es el CoolDown entre ataques
-    *Velocity es la velocidad de los enemigos
+    *Velocity es la velocidad de movimiento
+    *daño es el daño de cada ataque
+    *range es el rango de los ataques del player
+    *armor es la armadura del jugador
+    *Rage es el cargador de rabia
+    *eureka es el cargador de eureka
     */
-    constructor(scene, x, y, vida, damage, velocity)
+    constructor(scene, x, y, life, damage, velocity, range, armor, rage)
     {
         //super(scene, x ,y, ) hay que cargar la imagen con su id
 
-        this._vida = vida;
+        this._life = life;
         //Genera un Cool Down aleatorio ente los dos (distinto para cada enemigo), los números están puestos a vole
         //Aún no se si van milisegundos o segundos
         this._atkCD = Phaser.Math.FloatBetween(0.5, 1);
         this._velocity = velocity;
+        this._damage = damage;
+        this._range = range;
+        this._armor = armor;
+        this._rage = 0;
+        this._eureka = 0;
     }
-    
-    // métodos
+
+    //métodos
 
     //método para moverte
     mover = function(){
@@ -29,7 +38,13 @@ export default class Enemy extends Phaser.GameObjects.Sprite
     }
 
     //método para disparar
-    Disparar = function(){
+    disparar = function(){
         //vacío de momento
     }
+
+    // La dicotomía cambia el rango de ataque
+    cambiaRange = function(damageOffset){
+        //vacío de momento
+    }
+
 }
