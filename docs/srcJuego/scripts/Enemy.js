01e7f21de2d495063d026bf6af0cc52fc3c92bc9
samuel <samuga05@ucm.es>
2023-11-02 10:49:18 +0100

    Cambios en el enemy
    Constructor con param, enemy config y mas cosas

--- docs/srcJuego/scripts/Enemy.js
+++ docs/srcJuego/scripts/Enemy.js
@@ -3,22 +3,33 @@ export default class Enemy extends Phaser.GameObjects.Sprite
 {
     /*
     *Las variable con barra baja son las de la clase, si barra baja son las variables de la sobrecarga
-    *Scene es la escena a la que pertenecen los enemigos
-    *x es la coordenada x del sprite
-    *y es la coordenada y del sprite
-    *vida es la vida inicial del Enemigo
     *AtkCD es el CoolDown entre ataques
-    *Velocity es la velocidad de los enemigos
     */
-    constructor(scene, x, y, vida, damage, velocity)
-    {
-        //super(scene, x ,y, ) hay que cargar la imagen con su id
 
-        this._vida = vida;
+    /**
+ * Consturctor del player
+ * @param {Scene} scene escena a la que pertenecen los enemigos
+ * @param {number} x coordenada x del sprite
+ * @param {number} y coordenada y del sprite
+ * @param {number} playerConfig Objeto que guarda la informacion del enemy{life, velocity, damage,minCooldown,maxCooldown}
+ * */
+    constructor(scene, x, y,key,enemyConfig)
+    {
+        //constructor del padre
+        super(scene, x ,y,key); 
+
+        //atributos del enemigo
+        this._vida = enemyConfig.vida;
+        this._damage = enemyConfig.damage;
+        this._velocity = enemyConfig.velocity;
+
+        //cooldowns de ataque
+        this._minCD = enemyConfig.minCooldown;
+        this._maxCD = enemyConfig.maxCooldown;
+        
         //Genera un Cool Down aleatorio ente los dos (distinto para cada enemigo), los números están puestos a vole
         //Aún no se si van milisegundos o segundos
-        this._atkCD = Phaser.Math.FloatBetween(0.5, 1);
-        this._velocity = velocity;
+        this._atkCD = Phaser.Math.FloatBetween( this._minCD,this._maxCD);
     }
     
     // métodos
